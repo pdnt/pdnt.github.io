e672d88644c128fb544cf4747b549bf3570885ce
seq <pdntspa@protonmail.ch>
2023-07-28 16:39:31 -0300

    Fix path of deployment_counter.txt in script

--- scripts/counter.sh
+++ scripts/counter.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Load the current counter value from the file
-counter=$(cat ./deployment_counter.txt)
+counter=$(cat ./counter/deployment_counter.txt)
 # Increment the counter by 1
 new_counter=$((counter + 1))
 # Update the counter value in the file
-echo "$new_counter" > deployment_counter.txt
+echo "$new_counter" > ./counter/deployment_counter.txt
 # Store the new counter value as an output
 echo "::set-output name=counter_value::$new_counter"
